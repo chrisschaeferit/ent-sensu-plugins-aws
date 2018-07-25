a052cd7401afa724d5374c733fbd28109360b7e7
Chris Schaefer <cschaefer@entertainment.com>
2018-07-25 14:37:36 -0400

    working revision

--- bin/check-ec2-cpu_balance.rb
+++ bin/check-ec2-cpu_balance.rb
@@ -1,3 +1,4 @@
+
 #! /usr/bin/env ruby
 #
 # check-ec2-cpu_balance
@@ -32,7 +33,7 @@ require 'sensu-plugins-aws'
 require 'sensu-plugin/check/cli'
 require 'aws-sdk'
 require 'socket'
-
+require 'json'
 
 class EC2CpuBalance < Sensu::Plugin::Check::CLI
   include Common
@@ -84,19 +85,15 @@ class EC2CpuBalance < Sensu::Plugin::Check::CLI
   end
 
 
-  def instance_tag(instance, tag_name)
-    tag = instance.tags.select { |t| t.key == tag_name }.first
-    tag.nil? ? '' : tag.value
-  end
-  
   def send_client_socket(data)
     udp = UDPSocket.new
     udp.send(data + "\n", 0, '127.0.0.1', 3030)
   end
 
-def send_ok(check_name, msg)
+def send_ok(source_name, check_name, msg)
     event = {
       'name' => check_name,
+      'source' => source_name,
       'status' => 0,
       'output' => "#{self.class.name} OK: #{msg}",
       'handlers' => config[:handlers]
@@ -104,9 +101,11 @@ def send_ok(check_name, msg)
     send_client_socket(event.to_json)
   end
 
-def send_warning(check_name, msg)
+def send_warning(source_name, check_name, msg)
+
     event = {
       'name' => check_name,
+      'source' => source_name,
       'status' => 1,
       'output' => "#{self.class.name} WARNING: #{msg}",
       'handlers' => config[:handlers]
@@ -114,9 +113,10 @@ def send_warning(check_name, msg)
     send_client_socket(event.to_json)
   end
 
-  def send_critical(check_name, msg)
+  def send_critical(source_name, check_name, msg)
     event = {
-      'name' => check_name,
+     'name' => check_name,
+      'source' => source_name,
       'status' => 2,
       'output' => "#{self.class.name} CRITICAL: #{msg}",
       'handlers' => config[:handlers]
@@ -124,9 +124,10 @@ def send_warning(check_name, msg)
     send_client_socket(event.to_json)
   end
 
-  def send_unknown(check_name, msg)
+  def send_unknown(source_name, check_name, msg)
     event = {
       'name' => check_name,
+      'source' => source_name,
       'status' => 3,
       'output' => "#{self.class.name} UNKNOWN: #{msg}",
       'handlers' => config[:handlers]
@@ -144,7 +145,7 @@ def send_warning(check_name, msg)
         }
       ]
     )
-    vpcnames = ec2.describe_vpcs(
+   vpcnames = ec2.describe_vpcs(
             filters: [
                     {
                 name: 'tag-key',
@@ -153,38 +154,53 @@ def send_warning(check_name, msg)
             ]
     )
     vpc_fullname = vpcnames.data[:vpcs].first.tags.find{|tag| tag.key == 'Name' }.value
-    level = 0
-    
-    udp = UDPSocket.new
-    
+
+   level = 0
+
     instances.reservations.each do |reservation|
       reservation.instances.each do |instance|
         next unless instance.instance_type.start_with? 't2.'
         id = instance.instance_id
+        availzone = instance.placement.availability_zone
         private_addr = instance.private_ip_address
         result = data id
-        check_name = 'sensu-test'
         messages = "\n"
-        tag = config[:tag] ? " #{instance_tag(instance, config[:tag])}" : ''
-        output = "#{tag}-#{vpc_fullname}-#{private_addr}"
-        
+        tag = instance.tags.find{|tag| tag.key == 'Name'}.value
+        source_name = "#{tag}-#{vpc_fullname}-#{private_addr}"
+        check_name = "#{tag}_#{availzone}"
+        level = 3 if result.nil?
         unless result.nil?
           if result < config[:critical]
-          msg = "#{id}#{tag} is below critical threshold [#{result} < #{config[:critical]}]\n"
-           send_critical(check_name, msg)  
-           level = 2
-             
+          msg = "#{tag}-#{vpc_fullname}-#{private_addr} is below critical threshold [#{result} < #{config[:critical]}]\n"
+           send_critical(source_name, check_name, msg)
+           #level = 2
+
           elsif config[:warning] && result < config[:warning]
-           msg = "#{id}#{tag} is below warning threshold [#{result} < #{config[:warning]}]\n" 
-           send_warning(check_name, msg)
-           level = 1 if level.zero?
-            
+           msg = "#{tag}-#{vpc_fullname}-#{private_addr} is below warning threshold [#{result} < #{config[:warning]}]\n"
+           send_warning(source_name, check_name, msg)
+           #level = 1 if level.zero?
+
+   # if instances
+
+    if level == 0
+     msg = "All checks running"
+    end
+
+    if instances.reservations.nil?
+      level = 1
+      msg = "No instances checked in as running in #{config[:aws_region]}."
+    end
+
+    ok(msg) if level.zero?
+    warning(msg) if level == 1
+    critical(msg) if level == 2
+    unknown(msg) if level == 3
+
           end
         end
       end
     end
-    ok(output) if level.zero?
-    warning(output) if level == 1
-    critical(output) if level == 2
+
   end
 end
+
